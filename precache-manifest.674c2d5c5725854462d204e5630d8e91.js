9751fd72d8af96dbc438adb5ddaed7a9b96dedde
Bonsa-Tilahun <bonaatilahun@gmail.com>
2020-02-04 19:12:45 -0700

    Updates

---
+++ precache-manifest.674c2d5c5725854462d204e5630d8e91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a097f966e26ee7ef20296afcfde93c20",
+    "url": "/monster-roledex/index.html"
+  },
+  {
+    "revision": "a9a03b75b08d018fa742",
+    "url": "/monster-roledex/static/css/main.a48f8b7c.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monster-roledex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-roledex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9a03b75b08d018fa742",
+    "url": "/monster-roledex/static/js/main.254046ef.chunk.js"
+  },
+  {
+    "revision": "59ec43f1f378d3a69ba4",
+    "url": "/monster-roledex/static/js/runtime-main.8845442f.js"
+  }
+]);
